ad6ad496da91cfa094217a3832cb174577674c66
AlePatata <alejandra.campos.u@ug.uchile.cl>
2025-10-12 19:27:29 -0300

    psw hash instead of psw

--- backend/src/models/users.ts
+++ backend/src/models/users.ts
@@ -2,12 +2,11 @@ import mongoose from 'mongoose'
 
 
 export interface User {
-  id: string
-  username: string
-  email: string
-  password: string
-  // createdAt: Date
-  // updatedAt: Date
+  id: string;
+  username: string;
+  email: string;
+  passwordHash: string;
+  posts: mongoose.Types.ObjectId[];
 }
 
 
@@ -15,19 +14,22 @@ export interface MongooseUser {
   id?: string; 
   _id?: mongoose.Types.ObjectId; 
   __v?: number;
-  username: string
-  email: string
-  password: string
-  // createdAt: Date
-  // updatedAt: Date
+  username: string;
+  email: string;
+  passwordHash: string;
+  posts: mongoose.Types.ObjectId[];
 }
 
 const userSchema = new mongoose.Schema<User>({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  // createdAt: { type: Date, default: Date.now },
-  // updatedAt: { type: Date, default: Date.now },
+  passwordHash: { type: String, required: true },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
 }, { 
   timestamps: true 
 }); 
